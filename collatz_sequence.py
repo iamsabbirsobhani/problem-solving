e190e98ef8255328d0b1304c0b95d3b4ecc6928d
Sabbir Sobhani <sabbirsobhani@hotmail.com>
2021-06-18 23:25:25 +0600

    collatz sequence execution time reduced

--- collatz_sequence.py
+++ collatz_sequence.py
@@ -1,19 +1,14 @@
 # Python 3.8
-
-tCount = 0
-cD = 0
-m = 0
-a = 0
-for n in range(100, 0, -1):
-    m = n
-    c = 0
-    print("For {}".format(n)+" Collatz sequences: ")
+import time
+tCount, cD, m, a = 0, 0, 0, 0
+start = time.time()
+for n in range(1000000, 0, -1):
+    m, c = n, 0
     for i in range(10000):
         if n % 2 == 0:
             n = n/2
-        elif n % 2 is not 0:
+        elif n % 2 != 0:
             n = n * 3 + 1
-        print("    {}".format(n))
         if n >= 1:
             c = c + 1
         if n == 1:
@@ -24,9 +19,9 @@ for n in range(100, 0, -1):
         cD = m
         a = cD
         n = m
-    print("Total Collatz terms: {}".format(c))
-    print("\n")
 
+end = time.time()
 
 print("Starting number \"{}\" ".format(cD)+"produces the longest chain.")
 print("Sequence contains: {} ".format(tCount)+"terms.")
+print("Time elapsed: ", (end - start))
